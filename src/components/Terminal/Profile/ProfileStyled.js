2d3b42fe1f7285681d9d0069c8f65bacace9b145
David Lacarta <davidlacarta@gmail.com>
2019-07-14 10:22:15 +0200

    Add custom fonts

--- src/components/Terminal/Profile/ProfileStyled.js
+++ src/components/Terminal/Profile/ProfileStyled.js
@@ -4,8 +4,13 @@ import Color from "../../../utils/color";
 const ProfileStyled = styled.summary`
   word-break: break-all;
   font-size: 1rem;
+  padding: 1rem 0;
 
-  h1 > span {
+  h1 {
+    margin: 0;
+  }
+
+  span {
     padding-right: 1rem;
   }
 
@@ -16,6 +21,15 @@ const ProfileStyled = styled.summary`
 
   section > ul {
     padding-inline-start: 1rem;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+
+    li {
+      padding: 0.5rem;
+      display: flex;
+      align-items: center;
+    }
   }
 
   mark {
@@ -24,4 +38,13 @@ const ProfileStyled = styled.summary`
   }
 `;
 
-export { ProfileStyled };
+const SocialStyled = styled.p`
+  padding: 1rem 0;
+
+  a {
+    text-decoration: none;
+    color: unset;
+  }
+`;
+
+export { ProfileStyled, SocialStyled };
--- src/components/Terminal/Profile/index.jsx
+++ src/components/Terminal/Profile/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { ProfileStyled } from "./ProfileStyled";
+import { ProfileStyled, SocialStyled } from "./ProfileStyled";
 
 function Profile() {
   return (
@@ -13,24 +13,59 @@ function Profile() {
       </h1>
       <p>---------------------</p>
       <p>
-        <mark>OS</mark>: Frontend developer
+        <mark>Role</mark>: Frontend developer
       </p>
       <section>
-        <mark>Packages</mark>:
+        <mark>Skills</mark>:
         <ul>
-          <li>Javascript </li>
-          <li>CSS3 </li>
-          <li>React </li>
-          <li>Next </li>
-          <li>Gatsby</li>
+          <li>
+            <span class="icon-javascript" />
+            Javascript
+          </li>
+          <li>
+            <span class="icon-css3" />
+            CSS3
+          </li>
+          <li>
+            <span class="icon-react" />
+            React
+          </li>
+          <li>
+            <span class="icon-next-dot-js" />
+            Next
+          </li>
+          <li>
+            <span class="icon-gatsby" />
+            Gatsby
+          </li>
         </ul>
       </section>
       <p>
-        <mark>Uptime</mark>: 8 years
+        <mark>Experience</mark>: 8 years
       </p>
       <p>
-        <mark>Terminal</mark>: davidlacarta@gmail.com
+        <mark>Mail</mark>: davidlacarta@gmail.com
       </p>
+      <SocialStyled>
+        {[
+          {
+            href: "https://www.linkedin.com/in/davidlacarta/",
+            icon: "icon-linkedin"
+          },
+          {
+            href: "https://github.com/davidlacarta",
+            icon: "icon-github"
+          },
+          {
+            href: "https://www.linkedin.com/in/davidlacarta/",
+            icon: "icon-facebook"
+          }
+        ].map(({ href, icon }) => (
+          <a href={href} target="_blank" rel="noopener noreferrer">
+            <span class={icon} />
+          </a>
+        ))}
+      </SocialStyled>
     </ProfileStyled>
   );
 }
